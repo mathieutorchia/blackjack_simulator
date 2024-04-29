bcef114057534f5459bc413df8481eba90441eee
FatManTurkey <mathieutorchia@gmail.com>
2024-04-28 20:35:03 -0400

    General Changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def getCard(person_cards):
+def get_card(person_cards):
     """takes the persons cards (list) as input, and adds 1 card to their list, and adds the sum.
     It will also change the value of an 11 to 1 if total > 21"""
     card = random.choice(cards)
@@ -21,7 +21,7 @@ def getCard(person_cards):
     return total
 
 
-def hasEleven(list):
+def has_eleven(list):
     """Takes a list of cards as input, and return True if one of the cards is an eleven"""
     length = len(list)
     for i in range(0, length):
@@ -29,94 +29,126 @@ def hasEleven(list):
             return True
     return False
 
-
-def startGame(player_sum, dealer_sum):
+def start_game(player_sum, dealer_sum):
     """this takes the player and dealer sums, and then adds a card to their arrays, and returns the new sums"""
-    player_sum = getCard(player_cards)
-    player_sum = getCard(player_cards)
-    dealer_sum = getCard(dealer_cards)
+    player_sum = get_card(player_cards)
+    player_sum = get_card(player_cards)
+    dealer_sum = get_card(dealer_cards)
     # print(f"Dealer shows a {dealer_cards} for a total of {dealer_sum}.")
     # print(f"Player shows a {player_cards} for a total of {player_sum}.")
     return player_sum, dealer_sum
 
 
-# simple strategy:
-# (1) if dealer has less than 7, hit until you get to 12
-# (2) if dealer has 7 or more, hit until you get to 17
-
 # Running the entire simulation
 hands = 100
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-results = []
-money = 0
-for iteration in range(0, hands):
+results_global = []
+trips = 100
 
-    # Storing the player/dealer cards for the game
-    player_cards = []
-    dealer_cards = []
+for trip in range(0, trips):
+    money = 0
+    results_trip = []
+    for hand in range(0, hands):
 
-    # Storing the sum of the cards
-    player_sum = 0
-    dealer_sum = 0
+        # Storing the player/dealer cards for the game
+        player_cards = []
+        dealer_cards = []
 
-    # Storing a "W", "L" or "T"
-    result = ""
+        # Storing the sum of the cards
+        player_sum = 0
+        dealer_sum = 0
 
-    # Start of game
-    player_sum, dealer_sum = startGame(player_sum, dealer_sum)
-    player_first_cards = player_cards[0] + player_cards[1]
-    dealer_first_card = dealer_cards[0]
+        # Storing a "W", "L" or "T"
+        result = ""
 
-    # Player Strategy
-    if dealer_sum <= 6:
-        while player_sum <= 11:
-            player_sum = getCard(player_cards)
-    else:
-        while player_sum <= 16:
-            player_sum = getCard(player_cards)
+        # Start of game
+        player_sum, dealer_sum = start_game(player_sum, dealer_sum)
+        player_first_cards = player_cards[0] + player_cards[1]
+        dealer_first_card = dealer_cards[0]
 
-    # Dealer Strategy
-    while dealer_sum <= 16:
-        dealer_sum = getCard(dealer_cards)
+        # Player Strategy
+        if dealer_sum <= 6:
+            while player_sum <= 11:
+                player_sum = get_card(player_cards)
+        else:
+            while player_sum <= 16:
+                player_sum = get_card(player_cards)
 
-    # Record Results
-    if player_sum > 21:
-        result = "L"
-    elif dealer_sum > 21:
-        result = "W"
-    elif player_sum > dealer_sum:
-        result = "W"
-    elif player_sum < dealer_sum:
-        result = "L"
-    elif player_sum == dealer_sum:
-        result = "T"
+        # Dealer Strategy
+        while dealer_sum <= 16:
+            dealer_sum = get_card(dealer_cards)
 
-    if result == "W":
-        money += 1
-    elif result == "L":
-        money -= 1
+        # Record Results
+        if player_sum > 21:
+            result = "L"
+        elif dealer_sum > 21:
+            result = "W"
+        elif player_sum > dealer_sum:
+            result = "W"
+        elif player_sum < dealer_sum:
+            result = "L"
+        elif player_sum == dealer_sum:
+            result = "T"
 
-    # Appending some key factors from the game to the results list
-    results.append([iteration, player_first_cards, dealer_first_card, result, money])
+        if result == "W":
+            money += 1
+        elif result == "L":
+            money -= 1
 
-# Turning the results list into a dataframe
-df = pd.DataFrame(results, columns=['iteration','player_first_cards', 'dealer_first_card', 'result', 'money'])
-print(df)
+        # Appending some key factors from the game to the results list
+        results_trip.append([trip, hand, player_first_cards, dealer_first_card, result, money])
 
-# Counting the number of W, L, and T's
-value_counts = df['result'].value_counts()
-print(value_counts)
+    results_global.extend(results_trip)
 
-# Counting the percentage of times that we win based on the dealer upcard
-percentage_w_per_card = df.groupby('dealer_first_card')['result'].apply(lambda x: (x == 'W').mean() * 100)
-print(percentage_w_per_card)
+df = pd.DataFrame(results_global,
+                  columns=['trip', 'hand', 'player_first_cards', 'dealer_first_card', 'result', 'money'])
 
-# Plotting your bankroll over time
-plt.plot(df['iteration'], df['money'], marker = 'o', linestyle = '-')
-plt.xlabel('Iteration')
+# Split the DataFrame into separate trips
+trips_data = [group[1][['hand', 'money']] for group in df.groupby('trip')]
+
+# Plot each trip separately
+for trip_data in trips_data:
+    plt.plot(trip_data['hand'], trip_data['money'])
+
+# Set labels and title
+plt.xlabel('Hands')
 plt.ylabel('Money')
-plt.title('Plotting Money vs. Number of BlackJack Hands')
+plt.title(f'Money vs. Hands for {trips} Trips')
+
+# Show plot
 plt.show()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Counting the number of W, L, and T's
+# value_counts = df['result'].value_counts()
+# print(value_counts)
+#
+# # Counting the percentage of times that we win based on the dealer upcard
+# percentage_w_per_card = df.groupby('dealer_first_card')['result'].apply(lambda x: (x == 'W').mean() * 100)
+# print(percentage_w_per_card)
+#
+# # Plotting your bankroll over time
+# plt.plot(df['iteration'], df['money'], marker = 'o', linestyle = '-')
+# plt.xlabel('Iteration')
+# plt.ylabel('Money')
+# plt.title('Plotting Money vs. Number of BlackJack Hands')
+# plt.show()
